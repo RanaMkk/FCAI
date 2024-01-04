164615068b5438db5979a274754af47f00df9925
Rana Muhammed Ali <99143413+RanaMkk@users.noreply.github.com>
2024-01-04 18:32:33 +0200

    line methods c#

---
+++ Graphics/C# tasks/Lines/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Lines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(11, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 84);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(11, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 84);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(261, 67);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 10;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "dda";
+            series6.BorderWidth = 10;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "bersenham";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(837, 619);
+            this.chart1.TabIndex = 42;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(11, 345);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(132, 32);
+            this.checkBox2.TabIndex = 41;
+            this.checkBox2.Text = "Bersenham line";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(13, 276);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 32);
+            this.checkBox1.TabIndex = 40;
+            this.checkBox1.Text = "DDA line";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(185, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 24);
+            this.textBox3.TabIndex = 39;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(104, 222);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 24);
+            this.textBox4.TabIndex = 38;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 24);
+            this.textBox2.TabIndex = 37;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 24);
+            this.textBox1.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(202, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 22);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(112, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 22);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 22);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Point 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 22);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Point 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 22);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "write x and y for the line points";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 22);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Drawing lines using both DDA algorithm and Bersenham ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 710);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Graphics/C# tasks/Lines/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Lines
+{
+    public partial class Form1 : Form
+    {
+
+        static int GetOctant(int x1, int y1, int x2, int y2)
+        {
+            // check slope 
+            float slope = (float)(y2 - y1) / (x2 - x1);
+            Console.WriteLine("slope is " + slope);
+
+            int Octant;
+            // get the Octant
+            if (slope >= 0) // 1st , 2nd , 5th , 6th
+            {
+                if (slope <= 1) //1st , 5th
+                {
+                    if (x1 < x2) Octant = 1;
+                    else Octant = 5;
+                }
+                else // 2nd , 6th
+                {
+                    if (y1 < y2) Octant = 2;
+                    else Octant = 6;
+                }
+            }
+            else // 3rd , 4th , 7th , 8th
+            {
+                if (slope < -1) //3rd , 7th
+                {
+                    if (y1 < y2) Octant = 3;
+                    else Octant = 7;
+                }
+                else // 4th , 8th
+                {
+                    if (x1 > x2) Octant = 4;
+                    else Octant = 8;
+                }
+            }
+            return Octant;
+        }
+        static int[] Swap(int x, int y)
+        {
+            int[] temp = new int[2];
+            temp[0] = y;
+            temp[1] = x;
+            return temp;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Titles.Add("Graghic Lines");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            int x1 = Convert.ToInt32(textBox1.Text);
+            int y1 = Convert.ToInt32(textBox2.Text);
+            int x2 = Convert.ToInt32(textBox4.Text);
+            int y2 = Convert.ToInt32(textBox3.Text);
+            if (checkBox2.Checked == true)
+            {
+                int Octant = GetOctant(x1, y1, x2, y2);
+                Console.WriteLine("Octant is " + Octant);
+
+                // Get dx, dy, p0
+                int dx, dy, p0, Dc, Xk, Yk, Pk;
+                switch (Octant)
+                {
+                    case 1: //Without Changes
+                        dx = x2 - x1;
+                        dy = y2 - y1;
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Xk, Yk);
+                            if (Pk > 0)
+                            { Xk++; Yk++; Pk += Dc; }
+                            else
+                            { Xk++; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 2://Swap(x1,y1) & Swap(x2,y2)
+                        int[] Point1 = Swap(x1, y1);
+                        int[] Point2 = Swap(x2, y2);
+                        x1 = Point1[0]; y1 = Point1[1];
+                        x2 = Point2[0]; y2 = Point2[1];
+                        dx = x2 - x1;
+                        dy = y2 - y1;
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Yk, Xk);
+                            if (Pk > 0)
+                            { Xk++; Yk++; Pk += Dc; }
+                            else
+                            { Xk++; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+
+                    case 3://Swap(x1,y1) , Swap(x2,y2) , Set dy = -dy & Dectrment Y
+                        Point1 = Swap(x1, y1);
+                        Point2 = Swap(x2, y2);
+                        x1 = Point1[0]; y1 = Point1[1];
+                        x2 = Point2[0]; y2 = Point2[1];
+                        dx = x2 - x1;
+                        dy = (y2 - y1) * (-1);
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Yk, Xk);
+                            if (Pk > 0)
+                            { Xk++; Yk--; Pk += Dc; }
+                            else
+                            { Xk++; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 4://Set dx = -dx & Decrement X
+                        dx = (x2 - x1) * (-1);
+                        dy = y2 - y1;
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Xk, Yk);
+                            if (Pk > 0)
+                            { Xk--; Yk++; Pk += Dc; }
+                            else
+                            { Xk--; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 5://Set dx = -dx , Set dy = -dy , Decrement x & Decrement y
+                        dx = (x2 - x1) * (-1);
+                        dy = (y2 - y1) * (-1);
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Xk, Yk);
+                            if (Pk > 0)
+                            { Xk--; Yk--; Pk += Dc; }
+                            else
+                            { Xk--; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 6://Swap(x1,y1) , Swap(x2,y2) , Set dx = -dx , Set dy = -dy , Dectrment x & Dectrment Y
+                        Point1 = Swap(x1, y1);
+                        Point2 = Swap(x2, y2);
+                        x1 = Point1[0]; y1 = Point1[1];
+                        x2 = Point2[0]; y2 = Point2[1];
+                        dx = (x2 - x1) * (-1);
+                        dy = (y2 - y1) * (-1);
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Yk, Xk);
+                            if (Pk > 0)
+                            { Xk--; Yk--; Pk += Dc; }
+                            else
+                            { Xk--; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 7://Swap(x1,y1) , Swap(x2,y2) , Set dx = -dx & Dectrment x 
+                        Point1 = Swap(x1, y1);
+                        Point2 = Swap(x2, y2);
+                        x1 = Point1[0]; y1 = Point1[1];
+                        x2 = Point2[0]; y2 = Point2[1];
+                        dx = (x2 - x1) * (-1);
+                        dy = y2 - y1;
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Yk, Xk);
+                            if (Pk > 0)
+                            { Xk--; Yk++; Pk += Dc; }
+                            else
+                            { Xk--; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                    case 8://Set dy = -dy & Dectrment Y
+                        dx = x2 - x1;
+                        dy = (y2 - y1) * (-1);
+                        p0 = 2 * dy - dx;
+                        Dc = 2 * dy - 2 * dx;
+                        Xk = x1; Yk = y1; Pk = p0;
+                        while ((Xk != x2))
+                        {
+                            chart1.Series["bersenham"].Points.AddXY(Xk, Yk);
+                            if (Pk > 0)
+                            { Xk++; Yk--; Pk += Dc; }
+                            else
+                            { Xk++; Pk += 2 * dy; }
+                            Console.WriteLine("(" + Xk + "," + Yk + ")");
+                        }
+                        break;
+                }
+            }
+            if (checkBox1.Checked == true)
+            {
+                int dx = x2 - x1, dy = y2 - y1, steps;
+                float x_increment, y_increment, x = x1, y = y1;
+
+                steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+                x_increment = (float)dx / (float)steps;
+                y_increment = (float)dy / (float)steps;
+                chart1.Series["dda"].Points.AddXY((int)Math.Round(x), (int)Math.Round(y));
+                for (int k = 0; k < steps; k++)
+                {
+                    x += x_increment;
+                    y += y_increment;
+                    chart1.Series["dda"].Points.AddXY((int)Math.Round(x), (int)Math.Round(y));
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series["bersenham"].Points.Clear();
+            chart1.Series["dda"].Points.Clear();
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox4.Text = " ";
+            textBox3.Text = " ";
+        }
+
+    }
+}
